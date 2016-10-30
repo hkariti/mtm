4008c31ad2b22de97635478a80efc67f767e10f7
Hagai Kariti <hkariti@gmail.com>
2016-10-30 19:50:07 +0200

    first version of mem_tot

--- ex0/part1/mtm_tot.c
+++ ex0/part1/mtm_tot.c
@@ -1,1 +1,47 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
 
+#define BITS_IN_INT (sizeof(int)*8)
+int main() {
+    int numbers_count, exponents_sum, *numbers;
+    int exponent, set_bits, temp;
+
+    printf("Enter size of input:\n");
+    if ((scanf("%d", &numbers_count) != 1) || (numbers_count <= 0)) {
+        printf("Invalid size\n");
+        return 1;
+    }
+
+    numbers = malloc(numbers_count*sizeof(int));
+    printf("Enter numbers:\n");
+    for (int i=0; i < numbers_count; i++) {
+        if ((scanf("%d", &numbers[i]) != 1) || (numbers[i] <= 0)) {
+            printf("Invalid number\n");
+            free(numbers);
+            return 1;
+        }
+    }
+    exponents_sum = 0;
+    for (int i=0; i < numbers_count; i++) {
+        set_bits = 0;
+        exponent = 0;
+        temp = numbers[i];
+        for (int j=0; j < BITS_IN_INT; j++) {
+            if ((temp & 1) == 1) {
+                set_bits++;
+                exponent = j;
+            }
+            temp = temp >> 1;
+        }
+        if (set_bits == 1) {
+            printf("The number %d is a power of 2: %d = 2^%d\n", numbers[i], numbers[i], exponent);
+            exponents_sum += exponent;
+        }
+    }
+    printf("Total exponents sum is %d\n", exponents_sum);
+
+    free(numbers);
+    return 0;
+    
+}
