80ee5d48157c187b22b83bd16dc8482a28a1a60d
Or Avner <or@vsecgroup.com>
2016-11-08 15:38:57 +0200

    run_tests now ignoring all output files

--- ex1/run_tests.sh
+++ ex1/run_tests.sh
@@ -3,7 +3,7 @@
 cd PokemonGo
 export PATH=$PATH:../scripts
 for i in ../SampleTests/test*; do
-    [[ "$i" = *"expout" ]] && continue;
+    [[ "$i" = *"out" ]] && continue;
     echo Running `basename $i`
     bash $i > $i.out
     echo Diff for `basename $i`:
