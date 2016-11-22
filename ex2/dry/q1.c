28312a0e8cbd96f0aa542b8e52e15aff9d390a41
Or Avner <or@vsecgroup.com>
2016-11-22 16:37:53 +0200

    fixed errors: malloc, null-termination

--- ex2/dry/q1.c
+++ ex2/dry/q1.c
@@ -5,24 +5,20 @@
 
 char* partialCopyString(char* str, bool copy_even, int* out_len) {
     char* OUT;
+	*out_len = strlen(str) / 2;
     if (copy_even) {
-        out_len = malloc(sizeof(int));
-        *out_len = strlen(str) / 2;
-        OUT = malloc(*out_len);
+        OUT = malloc(*out_len + 1);
         for (int i = 0; i < strlen(str); i += 2) {
             OUT[i / 2] = str[i + 1];
         }
-    }
-    else
-    {
-        out_len = malloc(sizeof(int));
-        *out_len = strlen(str) / 2 + strlen(str) % 2;
-        OUT = malloc(*out_len);
-        for (int i = 0; i < strlen(str); i += 2)
-        {
+    } else {
+        *out_len += + strlen(str) % 2;
+        OUT = malloc(*out_len + 1);
+        for (int i = 0; i < strlen(str); i += 2) {
             OUT[i / 2] = str[i];
         }
     }
+	OUT[*out_len] = '\0';
     return OUT;
 }
 
@@ -35,6 +31,9 @@ int main(int argc, char **argv) {
         copy_even = true;
     else
         copy_even = false;
+
+	printf("Input String: %s\n", argv[1]);
+	printf("copy_even %d\n", copy_even);
 
     newstr = partialCopyString(argv[1], copy_even, &len);
     printf("Length is %d\n", len);
