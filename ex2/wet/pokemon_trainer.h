9d2f080c44af48dde7a42f22f4fdf625d58e1c42
Hagai Kariti <hkariti@gmail.com>
2016-11-30 16:51:37 +0200

    Document pokemon_trainer

--- ex2/wet/pokemon_trainer.h
+++ ex2/wet/pokemon_trainer.h
@@ -16,6 +16,9 @@ typedef enum {
 	POKEMON_TRAINER_DEPOSIT_FULL
 } PokemonTrainerResult;
 
+/*
+ * The pokemon list structure
+*/
 typedef struct pokemon_list_t {
     Pokemon* list;
     int length;
@@ -23,14 +26,40 @@ typedef struct pokemon_list_t {
     int min_length;
 } *PokemonList;
 
+/*
+ * The pokemon trainer structure
+*/
 typedef struct pokemon_trainer_t {
     char* name;
     PokemonList local_pokemon;
     PokemonList remote_pokemon;
 } *PokemonTrainer;
 
+/**
+* Creates a new trainer.
+*
+* The name parameter sets the name property.
+* initial_pokemon will be copied and put as the first element in the
+*   local_pokemon list.
+* max_num_local and max_num_remote will be used as the max_length for the
+*   local_pokemon and remote_pokemon lists, respectfully.
+*
+* @return
+* 	A new allocated trainer.
+* 	If name is NULL or empty; initial_pokemon is NULL; max_num_local or
+* 	  max_num_remote is not positive; or in case of a memory allocation failure
+* 	  - return NULL.
+**/
 PokemonTrainer pokemonTrainerCreate(char* name, Pokemon initial_pokemon,
 	int max_num_local, int max_num_remote);
+/**
+* Destroy a trainer.
+*
+* All of the given trainer's memory will be freed, including all of the local
+* and remote pokemon objects.
+*
+* If trainer is NULL, no action is taken.
+**/
 void pokemonTrainerDestroy(PokemonTrainer trainer);
 PokemonTrainer pokemonTrainerCopy(PokemonTrainer trainer);
 PokemonTrainerResult pokemonTrainerAddPokemon(PokemonTrainer trainer,
