8d45cfaaa0caeabd5efb9f6d8c64080a7f5b0944
Or Avner <or@vsecgroup.com>
2016-12-13 18:09:46 +0200

    changed Item functions names

--- ex3/Item.h
+++ ex3/Item.h
@@ -3,14 +3,14 @@
 
 typedef struct Item_t *Item;
 
-Item CreateItem(char* type, int value, int quantity);
-void DestroyItem(Item item);
-Item ItemCopy(Item item);
-int GetItemPrice(Item item);
-void IncreaseQuantity(Item item);
-void DecreaseQuantity(Item item); // Should return error if already at 0 or just keep it at 0?
-void ResetQuantity(Item item);
+Item createItem(char* type, int value, int quantity);
+void destroyITem(Item item);
+Item copyItem(Item item);
+void printItem(Item item);
+int getItemPrice(Item item);
+void increaseQuantity(Item item);
+void decreaseQuantity(Item item); // Should return error if already at 0 or just keep it at 0?
+void resetQuantity(Item item);
 bool isItemOver(Item item);
-void PrintItem(Item item);
 
 #endif 
