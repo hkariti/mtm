a15d07598ecb1d6fe56d7c03323e8039eabb7317
Or Avner <or@vsecgroup.com>
2016-12-20 16:53:39 +0200

    refactored store.h

--- ex3/store.h
+++ ex3/store.h
@@ -1,15 +1,15 @@
 #ifndef _STORE_H
 #define _STORE_H
-#include <map.h>
-#include <item.h>
+#include "map.h"
+#include "inventory.h"
 
 typedef Map Store;
 
 Store storeCreate();
 void storeDestroy(Store store);
-MtmErrorCode storeAddItem(Store store, Item item);
-MtmErrorCode storeRemoveItem(Store store, Item item);
-Item storeGetItem(Store store, Item item);
+MtmErrorCode storeAddItem(Store store, char* type, int value);
+MtmErrorCode storeBuytem(Store store, char* type, int value);
+int storeGetItemPrice(Store store, char* type, int value); //returns -1 if item doesnt exist
 void storePrintStock(Store store);
 
 #endif
