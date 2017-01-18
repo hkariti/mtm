9a0d088a5a078614ad82953763291728d192fefb
Or Avner <or@vsecgroup.com>
2017-01-18 20:42:27 +0200

    added gym class header and implementation

---
+++ ex4/gym.cc
@@ -0,0 +1,65 @@
+#include "gym.h"
+
+using namespace mtm::pokemongo;
+
+Trainer * Gym::PreferedTeamTrainer(Team team)
+{
+	Trainer* prefered_trainer = NULL;
+	for (Trainer* trainer : trainers_) {
+		if (trainer->GetTeam() == team) {
+			if (NULL == prefered_trainer) {
+				prefered_trainer = trainer;
+			}
+			if (trainer > prefered_trainer) {
+				prefered_trainer = trainer;
+			}
+		}
+	}
+	return prefered_trainer;
+}
+
+void Gym::Arrive(Trainer & trainer)
+{
+	Location::Arrive(trainer);
+	if (trainers_.size() == 1) {
+		leader = &trainer;
+	}
+	else if (leader->GetTeam() != trainer.GetTeam()) {
+		leader = TrainersBattle(*leader, trainer);
+	}
+}
+
+
+void Gym::Leave(Trainer & trainer)
+{
+	Location::Leave(trainer);
+	if (leader == &trainer) {
+		if (trainers_.empty()) {
+			//TODO: does the leader stay leader if has no replacement?
+			leader = NULL;
+			return;
+		}
+		leader = PreferedTeamTrainer(trainer.GetTeam());
+		if (NULL == leader) { //no trainers from the same team exist
+			Trainer* first_prefered = NULL;
+			Trainer* second_prefered = NULL;
+			for (int team = BLUE; team <= RED; team++) {
+				if (team != trainer.GetTeam()) {
+					if (NULL == first_prefered) {
+						first_prefered = PreferedTeamTrainer((Team)team);
+					}
+					else {
+						second_prefered = PreferedTeamTrainer((Team)team);
+					}
+				}
+			}
+			if (second_prefered == NULL) { // only one team exist in gym
+				leader =  first_prefered;
+			}
+			else { // 2 other teams exist in gym
+				leader = TrainersBattle(*first_prefered, *second_prefered);
+			}
+		}
+
+	}
+}
---
+++ ex4/gym.h
@@ -0,0 +1,32 @@
+#pragma once
+#include "location.h"
+#include "exceptions.h"
+#include "trainer.h"
+
+namespace mtm {
+namespace pokemongo {
+
+class Gym : public Location {
+
+	Trainer* leader;
+
+	// Get's pointer to the most prefered team trainer in gym
+	// return's NULL if no trainer for that team exist in gym
+	Trainer* PreferedTeamTrainer(Team team);
+
+public:
+
+	~Gym() {}
+	Gym(const Gym& gym) = default;
+	Gym& operator=(const Gym& gym) = default;
+
+	// throws LocationTrainerAlreadyInLocationException
+	void Arrive(Trainer& trainer) override;
+
+	// throws LocationTrainerNotFoundException
+	void Leave(Trainer& trainer) override;
+
+};
+
+}
+}
