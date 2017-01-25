efcdf91b117041a69348442e463c58b9b73dd006
Or Avner <or@vsecgroup.com>
2017-01-25 21:32:56 +0200

    added world & pokemon_go tests

---
+++ ex4/tests/pokemon_go_test.cc
@@ -0,0 +1,179 @@
+#include "../pokemon_go.h"
+
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <vector>
+
+#include "test_utils.h"
+#include "../trainer.h"
+
+using namespace mtm::pokemongo;
+using namespace std;
+
+void SetUpWorld(World* world) {
+	std::istringstream world_in("	GYM haifa\n\
+								POKESTOP tel_aviv POTION 5 CANDY 3 POTION 10\n\
+								STARBUCKS eilat pikachu 3.5 1 squirtle 4 2\n\
+								GYM ashdod\n\
+								POKESTOP ashkelon POTION 4\n\
+								STARBUCKS kfar_saba charmander 4 1 charizard 100 100");
+	string line;
+
+	while (std::getline(world_in, line)) {
+		istringstream line_stream(line);
+		line_stream >> *world;
+	}
+}
+
+
+bool testTrainerAdd() {
+	World* world = new World();
+	SetUpWorld(world);
+	PokemonGo pokemon_go(world);
+
+	// invalid args
+	ASSERT_THROW(PokemonGoInvalidArgsException,
+		pokemon_go.AddTrainer("", YELLOW, "aroma"));
+
+	// success
+	ASSERT_NO_THROW(pokemon_go.AddTrainer("ash", YELLOW, "eilat"));
+
+	// trainer already exist
+	ASSERT_THROW(PokemonGoTrainerNameAlreadyUsedExcpetion,
+		pokemon_go.AddTrainer("ash", YELLOW, "aroma"));
+
+	// location doesn't exist
+	ASSERT_THROW(PokemonGoLocationNotFoundException,
+		pokemon_go.AddTrainer("gary", YELLOW, "aroma"));
+	
+	return true;
+}
+
+bool testTrainerMove() {
+	World* world = new World();
+	SetUpWorld(world);
+	PokemonGo pokemon_go(world);
+
+	world->Connect("tel_aviv", "haifa", NORTH, SOUTH);
+	world->Connect("tel_aviv", "kfar_saba", EAST, WEST);
+	world->Connect("tel_aviv", "ashdod", SOUTH, NORTH);
+	world->Connect("ashdod", "ashkelon", SOUTH, NORTH);
+	world->Connect("ashkelon", "eilat", SOUTH, NORTH);
+	world->Connect("eilat", SOUTH);
+
+	pokemon_go.AddTrainer("ash", YELLOW, "eilat");
+
+	// trainer not found
+	ASSERT_THROW(PokemonGoTrainerNotFoundExcpetion,
+		pokemon_go.MoveTrainer("gary", WEST));
+
+	// location reached dead end
+	ASSERT_THROW(PokemonGoReachedDeadEndException,
+		pokemon_go.MoveTrainer("ash", WEST));
+
+	// success
+	ASSERT_NO_THROW(pokemon_go.MoveTrainer("ash", SOUTH));
+	ASSERT_EQUAL(pokemon_go.WhereIs("ash"), "eilat");
+	ASSERT_NO_THROW(pokemon_go.MoveTrainer("ash", NORTH));
+	ASSERT_EQUAL(pokemon_go.WhereIs("ash"), "ashkelon");
+
+	return true;
+}
+
+bool testWhereIs() {
+	World* world = new World();
+	SetUpWorld(world);
+	PokemonGo pokemon_go(world);
+
+	world->Connect("tel_aviv", "haifa", NORTH, SOUTH);
+	world->Connect("tel_aviv", "kfar_saba", EAST, WEST);
+	world->Connect("tel_aviv", "ashdod", SOUTH, NORTH);
+	world->Connect("ashdod", "ashkelon", SOUTH, NORTH);
+	world->Connect("ashkelon", "eilat", SOUTH, NORTH);
+	world->Connect("eilat", SOUTH);
+
+	pokemon_go.AddTrainer("ash", YELLOW, "eilat");
+
+	// invalid name
+	ASSERT_THROW(PokemonGoTrainerNotFoundExcpetion, 
+		pokemon_go.WhereIs("gary"));
+	ASSERT_THROW(PokemonGoTrainerNotFoundExcpetion, pokemon_go.WhereIs(""));
+
+	// success
+	ASSERT_EQUAL(pokemon_go.WhereIs("ash"), "eilat");
+
+	return true;
+}
+
+bool testTrainersIn() {
+	World* world = new World();
+	SetUpWorld(world);
+	PokemonGo pokemon_go(world);
+
+	world->Connect("tel_aviv", "haifa", NORTH, SOUTH);
+	world->Connect("tel_aviv", "kfar_saba", EAST, WEST);
+	world->Connect("tel_aviv", "ashdod", SOUTH, NORTH);
+	world->Connect("ashdod", "ashkelon", SOUTH, NORTH);
+	world->Connect("ashkelon", "eilat", SOUTH, NORTH);
+	world->Connect("eilat", SOUTH);
+
+	pokemon_go.AddTrainer("ash", YELLOW, "eilat");
+
+	// location doesnt exist
+	ASSERT_THROW(PokemonGoLocationNotFoundException,
+		pokemon_go.GetTrainersIn("aroma"));
+
+	ASSERT_TRUE(pokemon_go.GetTrainersIn("ashdod").empty());
+	ASSERT_EQUAL(pokemon_go.GetTrainersIn("eilat").size(), 1);
+
+	return true;
+}
+
+bool testGetScore() {
+	World* world = new World();
+	SetUpWorld(world);
+
+	world->Connect("tel_aviv", "haifa", NORTH, SOUTH);
+	world->Connect("tel_aviv", "kfar_saba", EAST, WEST);
+	world->Connect("tel_aviv", "ashdod", SOUTH, NORTH);
+	world->Connect("ashdod", "ashkelon", SOUTH, NORTH);
+	world->Connect("ashkelon", "eilat", SOUTH, NORTH);
+	world->Connect("eilat", SOUTH);
+
+	PokemonGo pokemon_go(world);
+	pokemon_go.AddTrainer("Ash", RED, "tel_aviv");
+
+	ASSERT_EQUAL(pokemon_go.GetScore(YELLOW), 0);
+	ASSERT_EQUAL(pokemon_go.GetScore(BLUE), 0);
+	ASSERT_EQUAL(pokemon_go.GetScore(RED), 1);
+
+	pokemon_go.MoveTrainer("Ash", SOUTH);
+
+	ASSERT_EQUAL(pokemon_go.GetScore(RED), 11);
+
+	pokemon_go.MoveTrainer("Ash", SOUTH);
+	pokemon_go.MoveTrainer("Ash", SOUTH);
+	pokemon_go.MoveTrainer("Ash", SOUTH);
+	pokemon_go.MoveTrainer("Ash", SOUTH);
+	pokemon_go.MoveTrainer("Ash", SOUTH);
+	pokemon_go.MoveTrainer("Ash", SOUTH);
+
+
+	pokemon_go.AddTrainer("Clemont", BLUE, "ashdod");
+	pokemon_go.MoveTrainer("Ash", NORTH);
+	pokemon_go.MoveTrainer("Ash", NORTH);
+
+	ASSERT_EQUAL(pokemon_go.GetScore(BLUE), 0);
+	ASSERT_EQUAL(pokemon_go.GetScore(RED), 14);
+
+	return true;
+}
+
+int main() {
+	RUN_TEST(testTrainerAdd);
+	RUN_TEST(testTrainerMove);
+	RUN_TEST(testWhereIs);
+	RUN_TEST(testTrainersIn);
+	RUN_TEST(testGetScore);
+}
---
+++ ex4/tests/world_test.cc
@@ -0,0 +1,68 @@
+#include "test_utils.h"
+#include "../world.h"
+#include "../exceptions.h"
+#include <string>
+#include <set>
+#include <fstream>
+#include <sstream>
+
+using namespace mtm::pokemongo;
+
+bool WorldInput() {
+	World world;
+
+	// Empty Line
+	std::istringstream input("");
+	ASSERT_THROW(WorldInvalidInputLineException, input >> world);
+
+	// Invalid location type
+	std::istringstream input_2("HOME sweet PIKACHU");
+	ASSERT_THROW(WorldInvalidInputLineException, input_2 >> world);
+	
+	// Gym Create
+	// too many args
+	std::istringstream input_3("GYM water_gym MISTY");
+	ASSERT_THROW(WorldInvalidInputLineException, input_3 >> world);
+	// no name
+	std::istringstream input_4("GYM  ");
+	ASSERT_THROW(WorldInvalidInputLineException, input_4 >> world);
+	// success
+	std::istringstream input_5("GYM water_gym");
+	ASSERT_NO_THROW(input_5 >> world);
+	
+	// Pokestop Create
+	std::istringstream input_6("POKESTOP 101_miles POTION 5 CHOCOLATE 3");
+	ASSERT_THROW(WorldInvalidInputLineException, input_6 >> world);
+	std::istringstream input_7("POKESTOP 101_miles POTION -5 CANDY 3");
+	ASSERT_THROW(WorldInvalidInputLineException, input_7 >> world);
+	std::istringstream input_8("POKESTOP 101_miles POTION 5 CANDY");
+	ASSERT_THROW(WorldInvalidInputLineException, input_8 >> world);
+	std::istringstream input_9("POKESTOP 101_miles POTION 5 CANDY 3 POTION 5 CANDY 3 POTION 5 CANDY 3 POTION 5 CANDY 3");
+	ASSERT_NO_THROW(input_9 >> world);
+
+	// Starbucks Create
+	// not enough args
+	std::istringstream input_10("STARBUCKS coffee_shop pikachu 2.5 3 pikachu 1 2 charmander 3.45");
+	ASSERT_THROW(WorldInvalidInputLineException, input_10 >> world);
+	// string as int
+	std::istringstream input_11("STARBUCKS coffee_shop pikachu 2.5 3 pikachu 1 2 123 3.45 4 3");
+	ASSERT_THROW(WorldInvalidInputLineException, input_11 >> world);
+	// int as string
+	std::istringstream input_12("STARBUCKS coffee_shop pikachu 2.5 3 pikachu 1 2 charmander squirtle 4 3");
+	ASSERT_THROW(WorldInvalidInputLineException, input_12 >> world);
+	// success
+	std::istringstream input_13("STARBUCKS coffee_shop pikachu 2.5 3 pikachu 1 2 charmander 3.45 4");
+	ASSERT_NO_THROW(input_13 >> world);
+
+	// location already exist
+	std::istringstream input_14("GYM coffee_shop");
+	ASSERT_THROW(WorldLocationNameAlreadyUsed, input_14 >> world);
+
+	return true;
+}
+
+int main() {
+  RUN_TEST(WorldInput);
+  return 0;
+}
+
