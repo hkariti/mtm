08d1631988d36242410d1668de3e140fdc0d565c
Hagai Kariti <hkariti@gmail.com>
2017-01-30 12:40:15 +0200

    Update dry q1

--- ex4/dry/q1.cpp
+++ ex4/dry/q1.cpp
@@ -3,21 +3,11 @@
 #include <iostream>
 
 /* Question 1 */
-template <class Iterator>
-class ContainerOperator {
-    public:
-    virtual ~ContainerOperator() {};
-    virtual bool operator()(const Iterator& begin1, Iterator const& end1,
-                        Iterator const& begin2, Iterator const& end2) const = 0;
-};
-
-template<typename T, class Iterator> 
-std::set<T> runAlg(Iterator begin, Iterator end,
-	ContainerOperator<Iterator>& op) {
-
+template<typename T, class Iterator, class Predicate> 
+std::set<T> runAlg(Iterator begin, Iterator end, Predicate& predicate) {
     std::set<T> filtered;
     for (Iterator it = begin; it != end; it++) {
-        if (op(begin, it, it, end)) {
+        if (predicate(begin, it, it, end)) {
             filtered.insert(*it);
         }
     }
@@ -25,12 +15,11 @@ std::set<T> runAlg(Iterator begin, Iterator end,
 }
 
 // Question 2
-template <class Iterator>
-class IsLarger : public ContainerOperator<Iterator> {
+class IsLarger {
     public:
-    ~IsLarger() {};
-    virtual bool operator()(const Iterator& begin1, Iterator const& end1,
-                        Iterator const& begin2, Iterator const& end2) const {
+    template <class Iterator>
+    bool operator()(const Iterator& begin1, Iterator const& end1,
+                    Iterator const& begin2, Iterator const& end2) const {
         if (begin1 == end1) return false;
         Iterator current, next;
         current = next = begin1;
@@ -41,11 +30,10 @@ class IsLarger : public ContainerOperator<Iterator> {
 };
 
 bool is_sorted(std::vector<int>& array) {
-    IsLarger<std::vector<int>::iterator> op;
+    IsLarger is_larger;
     std::set<int> filtered;
 
-    filtered = runAlg<int, std::vector<int>::iterator>(array.begin(),
-                                                       array.end(), op);
+    filtered = runAlg<int>(array.begin(), array.end(), is_larger);
     return filtered.empty();
 }
 
@@ -55,7 +43,7 @@ int main() {
     for (int i = 0; i < 10; i++) {
         vec.push_back(i);
     }
-    vec.push_back(1);
+//    vec.push_back(1);
 
     if (is_sorted(vec)) {
         cout << "sorted" << endl;
